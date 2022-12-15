47c368732b7c9415cfd3fb84a52e86a003ca109b
mcoen93ns <mathijs.coenen@ns.nl>
2022-12-15 09:00:18 +0100

    chore(grid): added function to create a grid

--- src/template.js
+++ src/template.js
@@ -1,5 +1,18 @@
+/* eslint-disable no-plusplus */
 function templateFunction(x) {
   return x / 2;
 }
 
-module.exports = templateFunction;
+// Create a grid of x by x cells and give all cells a value of 0
+function createGrid(x) {
+  const grid = [];
+  for (let i = 0; i < x; i++) {
+    grid[i] = [];
+    for (let j = 0; j < x; j++) {
+      grid[i][j] = 0;
+    }
+  }
+  return grid;
+}
+
+module.exports = { templateFunction, createGrid };
--- test/template.test.js
+++ test/template.test.js
@@ -1,7 +1,23 @@
-const templateFunction = require("../src/template");
+const { templateFunction, createGrid } = require("../src/template");
 
 describe("TemplateFunction should return half of the provided", () => {
   it("2 -> 1 ", () => {
     expect(templateFunction(2)).toEqual(1);
+  });
+});
+
+describe("CreateGrid should return a grid of x by x cells and give all cells a value of 0", () => {
+  it("2 -> [[0,0],[0,0]] ", () => {
+    expect(createGrid(2)).toEqual([
+      [0, 0],
+      [0, 0],
+    ]);
+  });
+  it("3 -> [[0,0,0],[0,0,0],[0,0,0]] ", () => {
+    expect(createGrid(3)).toEqual([
+      [0, 0, 0],
+      [0, 0, 0],
+      [0, 0, 0],
+    ]);
   });
 });
